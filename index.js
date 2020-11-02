470ff35f582e6b365c2cbde551b2ad0045dea523
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-02 23:42:11 +0530

    os file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { O_APPEND } = require("constants");
 const r=require("fs");
 //r.writeFileSync("file.txt","hello");
-const buf_data=r.readFileSync("file.txt");
+const buf_data=r.readFileSync("file.txt","utf-8");
 const o=buf_data.toString();
-console.log(buf_data.toString());
+console.log(buf_data);
---
+++ os/f1.js
@@ -0,0 +1,2 @@
+const os=require("os")
+console.log(os.arch());console.log(os.freemem()/1024/104/1024);console.log(os.totalmem()/1024/104/1024)
