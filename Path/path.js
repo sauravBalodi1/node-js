65ce440dfdfa1220ed31f41c5fac7299990d8889
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-02 23:54:44 +0530

    path overview

---
+++ Path/path.js
@@ -0,0 +1,4 @@
+const path=require("path");
+//console.log(path.dirname('C:/Users/SOURAVE/Desktop/Nodejs/Path/path.js'))
+//console.log(path.extname("C:/Users/SOURAVE/Desktop/Nodejs/Path/path.js"))
+//console.log(path.basename("C:/Users/SOURAVE/Desktop/Nodejs/Path/path.js"))
