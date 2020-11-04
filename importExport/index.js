b0b7e7eac8929471e6e418c3f1e72522ddcd164f
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-04 11:40:28 +0530

    how to create own modules

--- importExport/index.js
+++ importExport/index.js
@@ -1,2 +1,5 @@
-const add=require("./oper")
-console.log(add(5,5))//ab add function public ho gya hai
+const {add,sub}=require("./oper")
+//console.log(add(5,5))//ab add function public ho gya hai
+
+console.log(add(5,5));
+console.log(sub(5,5));
--- importExport/oper.js
+++ importExport/oper.js
@@ -1,4 +1,8 @@
 const add=(a,b)=>{
 return a+b;
 }
-module.exports=add;
+
+const sub=(a,b)=>{
+    return a-b;
+    }
+module.exports={add,sub}
