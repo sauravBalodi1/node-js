781a74a6f085274b979e8cce563c74b8afb5ea9e
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-05 21:29:52 +0530

    behind the scenes

---
+++ module_wrapper_function/f1.js
@@ -0,0 +1,7 @@
+(function(exports,require,module,__filename,__dirname){
+    const name='saurav'
+    console.log(name)
+})
+//yea advance javascript ka part hai, (IFFE),back end pe is tarike se kamm hota hai
+()//auto matic call bajegi
+console.log(__dirname)
