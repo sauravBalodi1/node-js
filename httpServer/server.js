834bb19db6596991a4fb8133f09cb20e07c33bd8
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-06 00:41:27 +0530

    Creating a server in nodeJS

---
+++ httpServer/server.js
@@ -0,0 +1,24 @@
+const http=require('http')
+const server=http.createServer((incomingMessage,serverResponse)=>{
+  //  console.log(incomingMessage.url)
+  if(incomingMessage.url=="/")
+    serverResponse.end("This i have created online server")
+    else if(incomingMessage.url=="/about")
+    serverResponse.end("routed to about page")
+    else if(incomingMessage.url=="/contact")
+    serverResponse.end("contact page")
+    else{
+     serverResponse.writeHead(404,{"Content-type":"text/html"}) 
+    serverResponse.end("<h1>404 error</h1>")
+    }
+})
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("the live server is created")
+    
+ })
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   const ip = res.socket.remoteAddress;
+//   const port = res.socket.remotePort;
+//   res.end(`Your IP address is ${ip} and your source port is ${port}.`);
+// }).listen(3000);
