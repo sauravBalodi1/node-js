a5c3261f8180cec6270305ce0e067fc7729fded7
sauravBalodi1 <sauravbalodi1234@gmail.com>
2020-11-10 01:47:39 +0530

    created a steam of Live file

---
+++ Streams/streamss.js
@@ -0,0 +1,24 @@
+//here we are creating a server and a file and as the modification will go on ,we will the live changes ,which we call as streaming
+const fs=require('fs')
+const http=require('http')
+const server=http.createServer();
+server.on('request',(req,res)=>{
+// fs.readFile("st.txt",(err,data)=>{
+//     if(err)return console.log(err)
+//     else
+//     res.end(data.toString())
+// })
+
+//THIS WAS MY OLD WAY,NOW NEXT M DOING STREAMING WAY
+const rstream=fs.createReadStream("st.txt")
+rstream.on('data',(chunkData)=>{//data is a property
+res.write(chunkData)
+})
+rstream.on('end',()=>{//data is a property
+res.end()
+})
+rstream.on("error",()=>{
+    res.end("file not found")
+})
+})
+server.listen(8000,"127.0.0.1")//isse server ko shuru kia hai
